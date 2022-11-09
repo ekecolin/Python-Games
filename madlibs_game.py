ba785bd01551aa7115d0fde79707ca624fb89672
ekecolin <117390062+ekecolin@users.noreply.github.com>
2022-11-09 22:59:57 +0000

    "First-madlib-complete"

---
+++ madlibs_game.py
@@ -0,0 +1,7 @@
+name = input('Enter the BVB player: ')
+team = input('Enter the German club: ')
+goalscorer = input('Enter the goalscorer: ')
+
+print(name + ' is my favourite BVB player')
+print('The 21/22 Bundesliga winners will be ' + team)
+print('The topscorer will be ' + goalscorer, 'with 35+ goals')
